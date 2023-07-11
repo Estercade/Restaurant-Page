7764034fb5941f5617e891e7ed92f4d07ca1352b
Estercade <withrwithoutyou@gmail.com>
2023-07-11 19:04:29 -0400

    Place landing functions in a module pattern

--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
-function renderHeader() {
-    const header = document.createElement('header');
+const landing = (function() {
+    const content = document.getElementById('content');
 
-    const logo = document.createElement('img');
-    logo.src = "./omakaselogo.png";
-    logo.alt = "O - Ma - Kase logo";
-    header.appendChild(logo);
-
-    return header;
-}
-
-function renderMainContent() {
-    const body = document.createElement('div');
-    const about = document.createElement('div');
-    const aboutHeader = document.createElement('h3');
-    aboutHeader.innerText = 'About';
-
-    const about1 = document.createElement('p');
-    about1.innerText = 'Omakase is a Japanese phrase which translates to "I leave it up to you." It is a term typically used in Japanese restaurants where the details of the meal are left up to the chef, who chooses seasonal specialties based on the highest quality ingredients available at the time.';
-
-    const about2 = document.createElement('p');
-    about2.innerText = 'O - Ma - Kase is a Japanese-style restaurant that aims to bring that authentic, personalized dining experience from the streets of Tokyo to your neighborhood.';
+    renderHeader = function() {
+        const header = document.createElement('header');
     
-    about.appendChild(aboutHeader);
-    about.appendChild(about1);
-    about.appendChild(about2);
+        const logo = document.createElement('img');
+        logo.src = "./omakaselogo.png";
+        logo.alt = "O - Ma - Kase logo";
+        header.appendChild(logo);
+    
+        return header;
+    };
 
-    body.appendChild(about);
+    renderMainContent = function() {
+        const body = document.createElement('div');
+        const about = document.createElement('div');
+        const aboutHeader = document.createElement('h3');
+        aboutHeader.innerText = 'About';
+    
+        const about1 = document.createElement('p');
+        about1.innerText = 'Omakase is a Japanese phrase which translates to "I leave it up to you." It is a term typically used in Japanese restaurants where the details of the meal are left up to the chef, who chooses seasonal specialties based on the highest quality ingredients available at the time.';
+    
+        const about2 = document.createElement('p');
+        about2.innerText = 'O - Ma - Kase is a Japanese-style restaurant that aims to bring that authentic, personalized dining experience from the streets of Tokyo to your neighborhood.';
+        
+        about.appendChild(aboutHeader);
+        about.appendChild(about1);
+        about.appendChild(about2);
+    
+        body.appendChild(about);
+    
+        return body;
+    };
 
-    return body;
-}
+    return { renderHeader, renderMainContent };
+})();
 
-const content = document.getElementById('content');
-
-content.appendChild(renderHeader());
-content.appendChild(renderMainContent());
+content.appendChild(landing.renderHeader());
+content.appendChild(landing.renderMainContent());
